485f1bf4f480041e0454350a7d0f7b68fc6f4c0f
ArtemAlt <artem.altunin.vit@gmail.com>
2021-08-13 14:45:02 +0300

    lesson_5

---
+++ src/main/java/hibernate/Main.java
@@ -0,0 +1,25 @@
+package hibernate;
+
+import hibernate.DAO.StudentDAO;
+import hibernate.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        StudentDAO dao = new StudentDAO();
+        List<Student> sl = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            sl.add(new Student("Student "+i,"mark"));
+        }
+        dao.persistList(sl);
+        System.out.println(dao.findById(500L));
+        System.out.println(dao.updateById(500L, "test1"));
+        System.out.println(dao.deleteById(501L));
+        System.out.println(dao.deleteAll());
+
+    }
+}
